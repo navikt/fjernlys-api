466a5955809aa1db3764c37f4ca68e1794bf30db
Christian Stensøe <christian.stensoe@nav.no>
2024-07-18 12:29:40 +0200

    moved serializable classes to dedicated file

--- src/main/kotlin/no/nav/fjernlys/plugins/Routing.kt
+++ src/main/kotlin/no/nav/fjernlys/plugins/Routing.kt
@@ -19,73 +19,7 @@ import javax.sql.DataSource
 
 fun Application.configureRouting(dataSource: DataSource) {
 
-    @Serializable
-    data class MeasureValue(
-        val category: String,
-        val status: String,
-    )
 
-    @Serializable
-    data class MeasureValueOut(
-        val id: String?,
-        val risk_assessment_id: String?,
-        val category: String,
-        val status: String,
-    )
-
-    @Serializable
-    data class RiskValueOut(
-        val id: String,
-        val probability: Double,
-        val consequence: Double,
-        val dependent: Boolean,
-        val riskLevel: String,
-        val category: String,
-        val measureValues: List<MeasureValueOut>?,
-        val newConsequence: Double? = null,
-        val newProbability: Double? = null
-    )
-
-    @Serializable
-    data class RiskValue(
-        val probability: Double,
-        val consequence: Double,
-        val dependent: Boolean,
-        val riskLevel: String,
-        val category: String,
-        val measureValues: List<MeasureValue>? = listOf(),
-        val newConsequence: Double? = null,
-        val newProbability: Double? = null
-    )
-
-    @Serializable
-    data class IncomingData(
-        val ownerData: Boolean,
-        val notOwnerData: String,
-        val serviceData: String,
-        val riskValues: List<RiskValue>
-    )
-
-    @Serializable
-    data class OutgoingData(
-        val id: String,
-        val is_owner: Boolean,
-        val owner_ident: String,
-        val service_name: String,
-        val risk_values: List<RiskValueOut>?,
-        val report_created: Instant,
-        val report_edited: Instant
-    )
-
-    @Serializable
-    data class RiskReportData(
-        val id: String,
-        val is_owner: Boolean,
-        val owner_ident: String,
-        val service_name: String,
-        val report_created: Instant,
-        val report_edited: Instant
-    )
 
     var incomingData: IncomingData? = null
     val currentMoment: Instant = Clock.System.now()

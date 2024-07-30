b55ddf43c992dc939ec3f10693104d3dea492e4b
mathamre <mathias.herbert.hamre@nav.no>
2024-07-30 10:45:53 +0200

    Fikset isAlive

--- src/main/kotlin/no/nav/fjernlys/appstatus/HealthRoute.kt
+++ src/main/kotlin/no/nav/fjernlys/appstatus/HealthRoute.kt
@@ -5,10 +5,10 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.health() {
-    get("internal/isalive") {
+    get("isalive") {
         call.respond("OK")
     }
-    get("internal/isready") {
+    get("isready") {
         call.respond("OK")
     }
 }
